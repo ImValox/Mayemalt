1f9c316a706de4b51813872b47760862239311c3
Valox <benji.brossier@gmail.com>
2024-12-04 18:08:50 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+    },
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    effect: 'slide',
+    
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      }
+    }
+  });
